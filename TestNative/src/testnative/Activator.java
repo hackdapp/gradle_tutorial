8c978b3ba1f343ecb030f4fa48b62264d86cd667
nolan.zhang <nolan.zhang@yunzhihui.com>
2015-02-13 10:53:06 +0800

    solve the problem that multi plugin load the nativelibrary

---
+++ TestNative/src/testnative/Activator.java
@@ -0,0 +1,47 @@
+package testnative;
+
+import org.hyperic.sigar.SigarProxy;
+import org.hyperic.sigar.SigarProxyCache;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		System.out.println("pid:" + getSigar().getPid());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+	private SigarProxy sigarProxy;
+
+	public SigarProxy getSigar() {
+		if (sigarProxy == null) {
+			sigarProxy = SigarProxyCache.newInstance();
+		}
+		return sigarProxy;
+	}
+
+}
